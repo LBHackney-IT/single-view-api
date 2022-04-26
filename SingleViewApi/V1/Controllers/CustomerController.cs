12ae3d347edd0470f7bf3d4e9c43e04cecf703e4
el <elena@madetech.com>
2022-04-26 10:47:19 +0100

    remove single view controller and return customer schema in consutomers
    Co-authored-by: Charli Wild <charli.wild@madetech.com>
    Co-authored-by: Jonathan Lewis <jonathan.lewis@madetech.com>

--- SingleViewApi/V1/Controllers/CustomerController.cs
+++ SingleViewApi/V1/Controllers/CustomerController.cs
@@ -27,7 +27,8 @@ namespace SingleViewApi.V1.Controllers
         /// </summary>
         /// <response code="200">...</response>
         /// <response code="400">Invalid Query Parameter.</response>
-        [ProducesResponseType(typeof(ResponseObjectList), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(CustomerResponseObject), StatusCodes.Status200OK)]
+
         [HttpGet]
         [LogCall(LogLevel.Information)]
         public IActionResult GetCustomer([FromQuery] string id, string userToken)
