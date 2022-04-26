06608c4901180fd1552861d504f64fe9ccfb6b48
Charli Wild <charli.wild@charlis-mbp.lan>
2022-04-26 13:35:21 +0100

    added class SearchResult

--- SingleViewApi/V1/Boundary/Response/SearchResponseObject.cs
+++ SingleViewApi/V1/Boundary/Response/SearchResponseObject.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 
 namespace SingleViewApi.V1.Boundary.Response
@@ -13,6 +14,25 @@ namespace SingleViewApi.V1.Boundary.Response
 
     {
         public HousingSearchApiResponse Response { get; set; }
+    }
+
+    public class SearchResult
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string FirstName { get; set; }
+        public string SurName { get; set; }
+        #nullable enable
+        public string? MiddleName { get; set; }
+        public string? PreferredFirstName { get; set; }
+        public string? PreferredSurname { get; set; }
+        #nullable disable
+        public string DateOfBirth { get; set; }
+        public List<string> PersonTypes { get; set; }
+        public bool IsPersonCautionaryAlert { get; set; }
+        public bool IsTenureCautionaryAlert { get; set; }
+        public List<KnownAddress> Addresses { get; set; }
+
     }
 
 
