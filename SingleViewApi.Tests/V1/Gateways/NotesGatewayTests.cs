5e77981e0fd63f03fa5e6145b655c1a7417d70fe
Jonathan Lewis <jonathan.lewis@madetech.com>
2022-04-29 11:47:47 +0100

    Refactor notes gateway test to use fixtures

--- SingleViewApi.Tests/V1/Gateways/NotesGatewayTests.cs
+++ SingleViewApi.Tests/V1/Gateways/NotesGatewayTests.cs
@@ -1,10 +1,12 @@
+using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
+using AutoFixture;
 using SingleViewApi.V1.Gateways;
-using FluentAssertions;
 using NUnit.Framework;
 using RichardSzalay.MockHttp;
+using SingleViewApi.V1.Boundary.Response;
 
 namespace SingleViewApi.Tests.V1.Gateways
 {
@@ -13,6 +15,7 @@ namespace SingleViewApi.Tests.V1.Gateways
     {
         private NotesGateway _classUnderTest;
         private MockHttpMessageHandler _mockHttp;
+        private Fixture _fixture;
 
         [SetUp]
         public void Setup()
@@ -21,13 +24,14 @@ namespace SingleViewApi.Tests.V1.Gateways
             const string baseUrl = "https://api.notes";
             var mockClient = _mockHttp.ToHttpClient();
             _classUnderTest = new NotesGateway(mockClient, baseUrl);
+            _fixture = new Fixture();
         }
 
         [Test]
         public void ARequestIsMade()
         {
-            const string id = "id";
-            const string userToken = "user-token";
+            var id = _fixture.Create<string>();
+            var userToken = _fixture.Create<string>();
 
             _mockHttp.Expect($"https://api.notes/notes?targetId={id}")
                 .WithHeaders("Authorization", userToken);
@@ -40,8 +44,8 @@ namespace SingleViewApi.Tests.V1.Gateways
         [Test]
         public async Task GetAllByIdReturnsNullIfNotesDoNotExist()
         {
-            const string id = "id";
-            const string userToken = "user-token";
+            var id = _fixture.Create<string>();
+            var userToken = _fixture.Create<string>();
 
             _mockHttp.Expect($"https://api.notes/notes?targetId={id}")
                 .WithHeaders("Authorization", userToken)
@@ -49,14 +53,14 @@ namespace SingleViewApi.Tests.V1.Gateways
 
             var notes = await _classUnderTest.GetAllById(id, userToken);
 
-            notes.Should().BeNull();
+            Assert.Null(notes);
         }
 
         [Test]
         public async Task GetAllByIdReturnsNullIfUserIsUnauthorised()
         {
-            const string id = "id";
-            const string userToken = "user-token";
+            var id = _fixture.Create<string>();
+            var userToken = _fixture.Create<string>();
 
             _mockHttp.Expect($"https://api.notes/notes?targetId={id}")
                 .WithHeaders("Authorization", userToken)
@@ -64,14 +68,14 @@ namespace SingleViewApi.Tests.V1.Gateways
 
             var notes = await _classUnderTest.GetAllById(id, userToken);
 
-            notes.Should().BeNull();
+            Assert.Null(notes);
         }
 
         [Test]
         public async Task GetAllByIdReturnsNullIfServiceUnavailable()
         {
-            const string id = "id";
-            const string userToken = "user-token";
+            var id = _fixture.Create<string>();
+            var userToken = _fixture.Create<string>();
 
             _mockHttp.Expect($"https://api.notes/notes?targetId={id}")
                 .WithHeaders("Authorization", userToken)
@@ -79,48 +83,30 @@ namespace SingleViewApi.Tests.V1.Gateways
 
             var notes = await _classUnderTest.GetAllById(id, userToken);
 
-            notes.Should().BeNull();
+            Assert.Null(notes);
         }
 
         [Test]
         public async Task DataIsReturned()
         {
-            const string id = "id";
-            const string userToken = "user-token";
+            var id = _fixture.Create<string>();
+            var userToken = _fixture.Create<string>();
+            var notesResultsResponseObject = new NotesResultsResponseObject() {Results = _fixture.CreateMany<NoteResponseObject>().ToList()};
 
             _mockHttp.Expect($"https://api.notes/notes?targetId={id}")
                 .WithHeaders("Authorization", userToken)
-                .Respond("application/json",
-                    "{\"results\": [" +
-                    "{\"id\": \"15c89eb9-ffc6-4ebe-9f1e-fcbb717d0ce4\", " +
-                    "\"title\": \"Test note 1\", " +
-                    "\"description\": \"Pluribus ad comitatum imperatoris vinctum perduceret.\", " +
-                    "\"targetType\": \"person\", " +
-                    "\"targetId\": \"e8a8aa89-0b57-ffdb-cb9f-cb4c3e5ed463\", " +
-                    "\"createdAt\": \"2022-04-14T15:08:13.765Z\", " +
-                    "\"categorisation\": {\"category\": \"repairs\", " +
-                    "\"subCategory\": null, \"description\": null}, " +
-                    "\"author\": {\"fullName\": \"Test user\", " +
-                    "\"email\": \"test.user@hackney.gov.uk\"}, " +
-                    "\"highlight\": false}, " +
-                    "{\"id\": \"c6407343-3607-422f-9a44-e4f44e55ee18\", " +
-                    "\"title\": \"Test note 2\", " +
-                    "\"description\": \"quo percitus ille exitio urgente abrupto ferro eundem adoritur paulum.\", " +
-                    "\"targetType\": \"person\", " +
-                    "\"targetId\": \"e8a8aa89-0b57-ffdb-cb9f-cb4c3e5ed463\", " +
-                    "\"createdAt\": \"2022-04-14T15:08:13.765Z\", " +
-                    "\"categorisation\": {\"category\": \"repairs\", " +
-                    "\"subCategory\": null, \"description\": null}, " +
-                    "\"author\": {\"fullName\": \"Test user\", " +
-                    "\"email\": \"test.user@hackney.gov.uk\"}, " +
-                    "\"highlight\": false}" +
-                    "]}");
+                .Respond("application/json", notesResultsResponseObject.ToJson());
 
             var results = await _classUnderTest.GetAllById(id, userToken);
 
             _mockHttp.VerifyNoOutstandingExpectation();
-            Assert.AreEqual(results.NoteResponseObjects[0].Title, "Test note 1");
-            Assert.AreEqual(results.NoteResponseObjects[1].Title, "Test note 2");
+
+            var noteResponseObject = notesResultsResponseObject.Results[^1];
+            var noteResponseObjectResult = results.NoteResponseObjects[^1];
+
+            Assert.AreEqual(noteResponseObject.Id, noteResponseObjectResult.Id);
+            Assert.AreEqual(noteResponseObject.Title, noteResponseObjectResult.Title);
+            Assert.AreEqual(noteResponseObject.Description, noteResponseObjectResult.Description);
         }
     }
 }
