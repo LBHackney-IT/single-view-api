10ab1ba245f411dac38a2f89e0e9583057b5f81a
Jonathan Lewis <jonathan.lewis@madetech.com>
2022-05-05 11:58:04 +0100

    Add CreateNote to gateway interface

--- SingleViewApi/V1/Gateways/INotesGateway.cs
+++ SingleViewApi/V1/Gateways/INotesGateway.cs
@@ -1,4 +1,5 @@
 using System.Threading.Tasks;
+using SingleViewApi.V1.Boundary.Request;
 using SingleViewApi.V1.Boundary.Response;
 
 namespace SingleViewApi.V1.Gateways
@@ -6,5 +7,7 @@ namespace SingleViewApi.V1.Gateways
     public interface INotesGateway
     {
         Task<NoteResponseObjectList> GetAllById(string targetId, string userToken, string paginationToken = null, int pageSize = 0);
+
+        Task<NoteResponseObject> CreateNote(string targetId, string userToken, CreateNoteRequest createNoteRequest);
     }
 }
