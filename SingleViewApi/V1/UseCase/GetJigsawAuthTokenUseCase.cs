bc7abb481d00c630e5286dca6d70bde2c81ed6a3
Charli Wild <charli.wild@charlis-mbp.lan>
2022-05-05 15:25:38 +0100

    added use case and controller for jigsaw

---
+++ SingleViewApi/V1/UseCase/GetJigsawAuthTokenUseCase.cs
@@ -0,0 +1,26 @@
+using System.Threading.Tasks;
+using Hackney.Core.Logging;
+using SingleViewApi.V1.Gateways;
+using SingleViewApi.V1.UseCase.Interfaces;
+
+namespace SingleViewApi.V1.UseCase
+{
+    public class GetJigsawAuthTokenUseCase: IGetJigsawAuthTokenUseCase
+    {
+        private IJigsawGateway _jigsawGateway;
+
+        public GetJigsawAuthTokenUseCase(IJigsawGateway jigsawGateway)
+        {
+            _jigsawGateway = jigsawGateway;
+        }
+
+        [LogCall]
+
+        public async Task<string> Execute(string username, string password)
+        {
+            var token = await _jigsawGateway.GetAuthToken(username, password);
+
+            return token;
+        }
+    }
+}
