862f219ae8e8326d676ca999ea0999a08dfead0e
Charli Wild <charli.wild@charlis-mbp.lan>
2022-05-12 11:47:43 +0100

    added controller and use case to store jwt

---
+++ SingleViewApi.Tests/V1/UseCase/StoreJigsawCredentialsUseCaseTests.cs
@@ -0,0 +1,40 @@
+using System;
+using Amazon.DynamoDBv2.Model;
+using FluentAssertions;
+using Hackney.Core.Testing.Shared;
+using Moq;
+using NUnit.Framework;
+using ServiceStack.Redis;
+using SingleViewApi.V1.Gateways;
+using SingleViewApi.V1.UseCase;
+
+namespace SingleViewApi.Tests.V1.UseCase;
+[TestFixture]
+public class StoreJigsawCredentialsUseCaseTests: LogCallAspectFixture
+{
+
+    private Mock<IRedisClient> _mockRedisClient;
+    private StoreJigsawCredentialsUseCase _classUnderTest;
+
+    [SetUp]
+    public void Setup()
+    {
+        _mockRedisClient = new Mock<IRedisClient>();
+        _classUnderTest = new StoreJigsawCredentialsUseCase(new RedisGateway(_mockRedisClient.Object));
+
+    }
+
+    [Test]
+    public void AddsValueAndReturnsId()
+    {
+        const string value = "testString";
+        const int ttl = 1;
+
+        _mockRedisClient.Setup(x => x.SetValue(It.IsAny<string>(), value, TimeSpan.FromDays(ttl)));
+
+        var response = _classUnderTest.Execute(value);
+
+        response.Should().BeOfType<string>();
+    }
+
+}
