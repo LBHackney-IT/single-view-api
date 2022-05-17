2b4e0425a048012bef195530be0782cf5d5600be
Charli Wild <charli.wild@charlis-mbp.home>
2022-05-17 16:31:20 +0100

    tdd approach for combining search results

--- SingleViewApi.Tests/V1/UseCase/GetCombinedSearchResultsByNameUseCaseTests.cs
+++ SingleViewApi.Tests/V1/UseCase/GetCombinedSearchResultsByNameUseCaseTests.cs
@@ -1,6 +1,10 @@
+using System.Collections.Generic;
 using AutoFixture;
+using FluentAssertions;
 using Moq;
 using NUnit.Framework;
+using SingleViewApi.V1.Boundary;
+using SingleViewApi.V1.Boundary.Response;
 using SingleViewApi.V1.UseCase;
 using SingleViewApi.V1.UseCase.Interfaces;
 
@@ -21,6 +25,49 @@ public class GetCombinedSearchResultsByNameUseCaseTests
         _mockGetSearchResultsByNameUseCase = new Mock<IGetSearchResultsByNameUseCase>();
         _classUnderTest = new GetCombinedSearchResultsByNameUseCase(_mockGetSearchResultsByNameUseCase.Object, _mockGetJigsawCustomersUseCase.Object);
         _fixture = new Fixture();
+    }
+
+    [Test]
+    public void ReturnsAnErrorWithSystemIdsWhenNoResultsFound()
+    {
+        var firstName = _fixture.Create<string>();
+        var lastName = _fixture.Create<string>();
+        var redisId = _fixture.Create<string>();
+        var userToken = _fixture.Create<string>();
+        var page = _fixture.Create<int>();
+
+        _mockGetSearchResultsByNameUseCase.Setup(x =>
+                x.Execute(firstName, lastName, page, userToken))
+            .ReturnsAsync(new SearchResponseObject()
+            {
+                SearchResponse = new SearchResponse()
+                {
+                    SearchResults = null,
+                    Total = 0,
+                },
+                SystemIds = new List<SystemId>(new[] { new SystemId() { SystemName = "HousingSearchApi" } })
+
+            });
+
+        _mockGetJigsawCustomersUseCase.Setup(x =>
+                x.Execute(firstName, lastName, redisId))
+            .ReturnsAsync(new SearchResponseObject()
+            {
+                SearchResponse = new SearchResponse()
+                {
+                    SearchResults = null,
+                    Total = 0,
+                },
+                SystemIds = new List<SystemId>(new[] { new SystemId() { SystemName = "Jigsaw" } })
+
+            });
+
+        var results = _classUnderTest.Execute(firstName, lastName, page, userToken, redisId).Result;
+
+        results.SystemIds[0].SystemName.Should().BeEquivalentTo("HousingSearchApi");
+        results.SystemIds[1].SystemName.Should().BeEquivalentTo("Jigsaw");
+
+
     }
 
 
