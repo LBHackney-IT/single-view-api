4f96ddc476ccee3bf5ea035e1b50995403b53af4
el <elena@madetech.com>
2022-05-17 16:26:59 +0100

    add logging to debug staging

--- SingleViewApi/V1/UseCase/StoreJigsawCredentialsUseCase.cs
+++ SingleViewApi/V1/UseCase/StoreJigsawCredentialsUseCase.cs
@@ -21,14 +21,21 @@ public class StoreJigsawCredentialsUseCase : IStoreJigsawCredentialsUseCase
 
     public string Execute(string encryptedCredentials)
     {
+        Console.WriteLine("--------- USECASE HIT ---------");
 
         var decryptedCredentials = _decoderHelper.DecodeJigsawCredentials(encryptedCredentials);
 
+        Console.WriteLine("--------- DECODED ---------");
+
         var token = _jigsawGateway.GetAuthToken(decryptedCredentials).Result;
+
+        Console.WriteLine("--------- GOT TOKEN ---------");
 
         if (String.IsNullOrEmpty(token)) return null;
 
         var id = _redisGateway.AddValue(encryptedCredentials, 1);
+
+        Console.WriteLine("--------- GOT ID ---------");
 
         return id;
     }
