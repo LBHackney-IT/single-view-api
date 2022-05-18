587f616395b68516a415478090fcbe01dcf803a0
Charli Wild <charli.wild@charlis-mbp.home>
2022-05-18 10:48:09 +0100

    added tests and code to concatenate results

--- SingleViewApi.Tests/V1/UseCase/GetCombinedSearchResultsByNameUseCaseTests.cs
+++ SingleViewApi.Tests/V1/UseCase/GetCombinedSearchResultsByNameUseCaseTests.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 using AutoFixture;
 using FluentAssertions;
 using Moq;
@@ -32,9 +33,15 @@ public class GetCombinedSearchResultsByNameUseCaseTests
     {
         var firstName = _fixture.Create<string>();
         var lastName = _fixture.Create<string>();
+        var searchTerm = $"{firstName}+{lastName}";
         var redisId = _fixture.Create<string>();
         var userToken = _fixture.Create<string>();
         var page = _fixture.Create<int>();
+        var expectedSystemIds = new List<SystemId>
+        {
+            new SystemId() { Id = searchTerm, SystemName = "HousingSearchApi", Error = "no results found" },
+            new SystemId(){ Id = searchTerm, SystemName = "Jigsaw", Error = "no results found" }
+        };
 
         _mockGetSearchResultsByNameUseCase.Setup(x =>
                 x.Execute(firstName, lastName, page, userToken))
@@ -45,7 +52,7 @@ public class GetCombinedSearchResultsByNameUseCaseTests
                     SearchResults = null,
                     Total = 0,
                 },
-                SystemIds = new List<SystemId>(new[] { new SystemId() { SystemName = "HousingSearchApi" } })
+                SystemIds = new List<SystemId>(new[] { new SystemId() { SystemName = "HousingSearchApi", Id = searchTerm, Error = "no results found"} })
 
             });
 
@@ -58,14 +65,71 @@ public class GetCombinedSearchResultsByNameUseCaseTests
                     SearchResults = null,
                     Total = 0,
                 },
-                SystemIds = new List<SystemId>(new[] { new SystemId() { SystemName = "Jigsaw" } })
+                SystemIds = new List<SystemId>(new[] { new SystemId() { SystemName = "Jigsaw", Id = searchTerm, Error = "no results found"} })
 
             });
 
         var results = _classUnderTest.Execute(firstName, lastName, page, userToken, redisId).Result;
 
-        results.SystemIds[0].SystemName.Should().BeEquivalentTo("HousingSearchApi");
-        results.SystemIds[1].SystemName.Should().BeEquivalentTo("Jigsaw");
+        results.SystemIds.Should().BeEquivalentTo(expectedSystemIds);
+
+    }
+
+    [Test]
+    public void ReturnsConcatenatedSearchResults()
+    {
+        var firstName = _fixture.Create<string>();
+        var lastName = _fixture.Create<string>();
+        var redisId = _fixture.Create<string>();
+        var userToken = _fixture.Create<string>();
+        var page = _fixture.Create<int>();
+        var jigsawResults = _fixture.Create<SearchResponse>();
+        var housingResults = _fixture.Create<SearchResponse>();
+        var jigsawResponseObject = new SearchResponseObject()
+        {
+            SearchResponse = jigsawResults,
+            SystemIds = new List<SystemId>()
+            {
+                new SystemId() { SystemName = "Jigsaw", Id = $"{firstName}+{lastName}" }
+            }
+        };
+        var housingResponseObject = new SearchResponseObject()
+        {
+            SearchResponse = housingResults,
+            SystemIds = new List<SystemId>()
+            {
+                new SystemId() { SystemName = "HousingSearchApi", Id = $"{firstName}+{lastName}" }
+            }
+        };
+        var expectedSearchResults = new SearchResponseObject()
+        {
+            SearchResponse = new SearchResponse()
+            {
+                SearchResults =
+                    housingResults.SearchResults
+                        .Concat(jigsawResults.SearchResults).ToList(),
+                Total = housingResults.Total + jigsawResults.Total
+            },
+            SystemIds = new List<SystemId>()
+            {
+                new SystemId() { SystemName = "Jigsaw", Id = $"{firstName}+{lastName}" },
+                new SystemId() { SystemName = "HousingSearchApi", Id = $"{firstName}+{lastName}" }
+            }
+        };
+
+        _mockGetSearchResultsByNameUseCase.Setup(x =>
+                x.Execute(firstName, lastName, page, userToken))
+            .ReturnsAsync(housingResponseObject);
+
+        _mockGetJigsawCustomersUseCase.Setup(x =>
+                x.Execute(firstName, lastName, redisId))
+            .ReturnsAsync(jigsawResponseObject);
+
+        var results = _classUnderTest.Execute(firstName, lastName, page, userToken, redisId).Result;
+
+        results.Should().BeEquivalentTo(expectedSearchResults);
+
+
 
 
     }
