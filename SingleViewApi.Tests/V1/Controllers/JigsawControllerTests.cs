9004d775680a685fec0706a3ae9d0fef6e773180
Charli Wild <charli.wild@charlis-mbp.home>
2022-05-20 14:11:47 +0100

    added controller for new endpoint

---
+++ SingleViewApi.Tests/V1/Controllers/JigsawControllerTests.cs
@@ -0,0 +1,32 @@
+using Hackney.Core.Testing.Shared;
+using Moq;
+using NUnit.Framework;
+using SingleViewApi.V1.Controllers;
+using SingleViewApi.V1.UseCase.Interfaces;
+
+namespace SingleViewApi.Tests.V1.Controllers;
+
+[TestFixture]
+public class JigsawControllerTests : LogCallAspectFixture
+{
+    private JigsawController _classUnderTest;
+    private Mock<IGetJigsawCustomerByIdUseCase> _getJigsawCustomerByIdUseCaseMock;
+
+    [SetUp]
+    public void SetUp()
+    {
+        _getJigsawCustomerByIdUseCaseMock = new Mock<IGetJigsawCustomerByIdUseCase>();
+        _classUnderTest = new JigsawController(_getJigsawCustomerByIdUseCaseMock.Object);
+    }
+
+    [Test]
+    public void UseCaseGetsCalled()
+    {
+        const string id = "test-id";
+        const string redisId = "test-redis-id";
+
+        _ = _classUnderTest.GetCustomerById(id, redisId);
+
+        _getJigsawCustomerByIdUseCaseMock.Verify(x => x.Execute(id, redisId), Times.Once);
+    }
+}
