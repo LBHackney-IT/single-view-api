bc727f9d6e8d89b5b0e438ec1fa887601c85adf4
el <elena@madetech.com>
2022-05-23 12:46:24 +0100

    add debugging

--- SingleViewApi/V1/Gateways/JigsawGateway.cs
+++ SingleViewApi/V1/Gateways/JigsawGateway.cs
@@ -93,6 +93,9 @@ namespace SingleViewApi.V1.Gateways
             {
                 var jsonBody = response.Content.ReadAsStringAsync().Result;
 
+                Console.WriteLine($"------> GOT RESULTS {jsonBody}. PARSING...");
+
+
                 searchResults = JsonConvert.DeserializeObject<List<JigsawCustomerSearchApiResponseObject>>(jsonBody);
 
             }
