5968d2391c65a793a90330cf864a5a1d4a20e2e5
Jonathan Lewis <jonathan.lewis@madetech.com>
2022-05-26 15:57:55 +0100

    Refactor DataSource to an enum

--- SingleViewApi/V1/Boundary/SystemId.cs
+++ SingleViewApi/V1/Boundary/SystemId.cs
@@ -1,9 +1,11 @@
+using SingleViewApi.V1.Domain;
+
 namespace SingleViewApi.V1.Boundary
 {
     public class SystemId
     {
         public static readonly string NotFoundMessage = "Not found";
-        public string SystemName { get; set; }
+        public DataSource SystemName { get; set; }
         public string Id { get; set; }
 #nullable enable
         public string? Error { get; set; }
