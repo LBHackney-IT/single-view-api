e3ab33ee3a89390e98e51d26ffd2cbe95ee3461a
el <elena@madetech.com>
2022-05-30 14:44:46 +0100

    change rds aws module

--- terraform/development/main.tf
+++ terraform/development/main.tf
@@ -109,68 +109,66 @@ module "security_group" {
     ]
 }
 
-################################################################################
-# RDS Module
-################################################################################
-module "rds" {
+##############################################################
+# Data sources to get VPC, subnets and security group details
+##############################################################
+data "aws_subnet_ids" "all" {
+    vpc_id = local.vpc_id
+}
+
+data "aws_security_group" "default" {
+    vpc_id = local.vpc_id
+    name   = "default"
+}
+
+#####
+# DB
+#####
+module "db" {
     source  = "terraform-aws-modules/rds/aws"
-    version = "4.4.0"
-    # insert the 38 required variables here
+    version = "2.0.0"
 
     identifier = local.application_name
 
-    # All available versions: https://docs.aws.amazon.com/AmazonRDS/latest/UserGuide/CHAP_PostgreSQL.html#PostgreSQL.Concepts
-    engine               = "postgres"
-    engine_version       = "14.1"
-    family               = "postgres14" # DB parameter group
-    major_engine_version = "14"         # DB option group
-    instance_class       = "db.t2.micro"
+    engine            = "postgres"
+    engine_version    = "9.6.9"
+    instance_class    = "db.t2.micro"
+    allocated_storage = 5
+    storage_encrypted = false
 
-    allocated_storage     = 20
-    max_allocated_storage = 100
+    # kms_key_id        = "arm:aws:kms:<region>:<account id>:key/<kms key id>"
+    name = local.application_name
 
     # NOTE: Do NOT use 'user' as the value for 'username' as it throws:
     # "Error creating DB Instance: InvalidParameterValue: MasterUsername
     # user cannot be used as it is a reserved word used by the engine"
-    db_name  = "completePostgresql"
-    username = "complete_postgresql"
-    port     = 5432
+    username = "dbuser"
 
-    multi_az               = true
-    db_subnet_group_name   = aws_elasticache_subnet_group.default.name
-    vpc_security_group_ids = [module.security_group.security_group_id]
+    password = "YourPwdShouldBeLongAndSecure!"
+    port     = "5432"
 
-    maintenance_window              = "Mon:00:00-Mon:03:00"
-    backup_window                   = "03:00-06:00"
-    enabled_cloudwatch_logs_exports = ["postgresql", "upgrade"]
-    create_cloudwatch_log_group     = true
+    vpc_security_group_ids = [data.aws_security_group.default.id]
 
+    maintenance_window = "Mon:00:00-Mon:03:00"
+    backup_window      = "03:00-06:00"
+
+    # disable backups to create DB faster
     backup_retention_period = 0
-    skip_final_snapshot     = true
-    deletion_protection     = false
 
-    performance_insights_enabled          = true
-    performance_insights_retention_period = 7
-    create_monitoring_role                = true
-    monitoring_interval                   = 60
-    monitoring_role_name                  = "single-view-monitoring-role-name"
-    monitoring_role_description           = "Description for monitoring role"
+    enabled_cloudwatch_logs_exports = ["postgresql", "upgrade"]
 
-    parameters = [
-        {
-            name  = "autovacuum"
-            value = 1
-        },
-        {
-            name  = "client_encoding"
-            value = "utf8"
-        }
-    ]
+    # DB subnet group
+    subnet_ids = data.aws_subnet_ids.all.ids
 
-    db_option_group_tags = {
-        "Sensitive" = "low"
-    }
-    db_parameter_group_tags = {
-        "Sensitive" = "low"
-    }
+    # DB parameter group
+    family = "postgres9.6"
+
+    # DB option group
+    major_engine_version = "9.6"
+
+    # Snapshot name upon DB deletion
+    final_snapshot_identifier = "demodb"
+
+    # Database Deletion Protection
+    deletion_protection = false
 }
