ec59d148a43f1b0dbcd711df793574d0679538b1
Jonathan Lewis <jonathan.lewis@madetech.com>
2022-06-07 16:14:59 +0100

    Add SingleViewContext test

---
+++ SingleViewApi.Tests/V1/Infrastructure/SingleViewContextTest.cs
@@ -0,0 +1,24 @@
+using System.Linq;
+using SingleViewApi.Tests.V1.Helper;
+using NUnit.Framework;
+
+namespace SingleViewApi.Tests.V1.Infrastructure
+{
+    [TestFixture]
+    public class SingleViewContextTest : DatabaseTests
+    {
+        [Test]
+        public void CanGetADataSourceDbEntity()
+        {
+            var dataSourceDbEntity = DataSourceDbEntityHelper.CreateDatabaseEntity();
+
+            SingleViewContext.Add(dataSourceDbEntity);
+            SingleViewContext.SaveChanges();
+
+            var result = SingleViewContext.DataSources.ToList().LastOrDefault();
+
+            Assert.AreEqual(result.Id, dataSourceDbEntity.Id);
+            Assert.AreEqual(result.Name, dataSourceDbEntity.Name);
+        }
+    }
+}
