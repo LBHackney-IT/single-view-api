7ee823c20b079d6142516099443d9030f44a41cb
Charli Wild <charli.wild@Charlis-MacBook-Pro.local>
2022-06-16 14:35:32 +0100

    added response object class

--- SingleViewApi/V1/Gateways/IEqualityInformationGateway.cs
+++ SingleViewApi/V1/Gateways/IEqualityInformationGateway.cs
@@ -1,8 +1,9 @@
 using System.Threading.Tasks;
+using SingleViewApi.V1.Boundary.Response;
 
 namespace SingleViewApi.V1.Gateways;
 
 public interface IEqualityInformationGateway
 {
-    Task<dynamic> GetEqualityInformationById(string id, string userToken);
+    Task<EqualityInformationResponseObject> GetEqualityInformationById(string id, string userToken);
 }
