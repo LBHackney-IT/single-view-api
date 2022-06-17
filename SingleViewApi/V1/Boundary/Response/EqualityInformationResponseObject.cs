7bce9f5757fde66148eb84c556a181dbfd7326ab
Charli Wild <charli.wild@charlis-mbp.lan>
2022-06-17 16:29:14 +0100

    added nullable to equality response object

--- SingleViewApi/V1/Boundary/Response/EqualityInformationResponseObject.cs
+++ SingleViewApi/V1/Boundary/Response/EqualityInformationResponseObject.cs
@@ -69,22 +69,24 @@ public class EqualityInformationResponseObject
 {
     public string Id { get; set; }
     public string TargetId { get; set; }
-    public string AgeGroup { get; set; }
-    public Gender Gender { get; set; }
-    public string Nationality { get; set; }
-    public Ethnicity Ethnicity { get; set; }
-    public ReligionOrBelief ReligionOrBelief { get; set; }
-    public SexualOrientation SexualOrientation { get; set; }
-    public MarriageOrCivilPartnership MarriageOrCivilPartnership { get; set; }
-    public List<PregnancyOrMaternity> PregnancyOrMaternity { get; set; }
-    public string NationalInsuranceNumber { get; set; }
-    public List<Language> Languages { get; set; }
-    public CaringResponsibilities CaringResponsibilities { get; set; }
-    public bool Disabled { get; set; }
-    public List<string> CommunicationRequirements { get; set; }
-    public EconomicSituation EconomicSituation { get; set; }
-    public HomeSituation HomeSituation { get; set; }
-    public string ArmedForces { get; set; }
+    #nullable enable
+    public string? AgeGroup { get; set; }
+    public Gender? Gender { get; set; }
+    public string? Nationality { get; set; }
+    public Ethnicity? Ethnicity { get; set; }
+    public ReligionOrBelief? ReligionOrBelief { get; set; }
+    public SexualOrientation? SexualOrientation { get; set; }
+    public MarriageOrCivilPartnership? MarriageOrCivilPartnership { get; set; }
+    public List<PregnancyOrMaternity>? PregnancyOrMaternity { get; set; }
+    public string? NationalInsuranceNumber { get; set; }
+    public List<Language>? Languages { get; set; }
+    public CaringResponsibilities? CaringResponsibilities { get; set; }
+    public bool? Disabled { get; set; }
+    public List<string>? CommunicationRequirements { get; set; }
+    public EconomicSituation? EconomicSituation { get; set; }
+    public HomeSituation? HomeSituation { get; set; }
+    public string? ArmedForces { get; set; }
+    #nullable disable
 }
 
 
