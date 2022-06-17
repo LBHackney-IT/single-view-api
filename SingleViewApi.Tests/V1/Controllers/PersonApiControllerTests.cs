4dd7fb98939c61a6f43e257299651ca4cfd9a57a
el <elena@madetech.com>
2022-06-17 12:11:06 +0100

    add GET /api/v1/getPersonApiCustomer endpoint

---
+++ SingleViewApi.Tests/V1/Controllers/PersonApiControllerTests.cs
@@ -0,0 +1,32 @@
+using Hackney.Core.Testing.Shared;
+using Moq;
+using NUnit.Framework;
+using SingleViewApi.V1.Controllers;
+using SingleViewApi.V1.UseCase.Interfaces;
+
+namespace SingleViewApi.Tests.V1.Controllers;
+
+[TestFixture]
+public class PersonApiControllerTests : LogCallAspectFixture
+{
+    private PersonApiController _classUnderTest;
+    private Mock<IGetPersonApiByIdUseCase> _getPersonApiByIdUseCaseUseCaseMock;
+
+    [SetUp]
+    public void SetUp()
+    {
+        _getPersonApiByIdUseCaseUseCaseMock = new Mock<IGetPersonApiByIdUseCase>();
+        _classUnderTest = new PersonApiController(_getPersonApiByIdUseCaseUseCaseMock.Object);
+    }
+
+    [Test]
+    public void UseCaseGetsCalled()
+    {
+        const string id = "test-id";
+        const string hackneyToken = "test-token";
+
+        _ = _classUnderTest.GetPersonApiCustomer(id, hackneyToken);
+
+        _getPersonApiByIdUseCaseUseCaseMock.Verify(x => x.Execute(id, hackneyToken), Times.Once);
+    }
+}
