d5a6fa3f6121166fad70b59af8669df7cee046ba
Charli Wild <charli.wild@charlis-mbp.lan>
2022-06-28 16:10:20 +0100

    completed tests for the academy gateway search endpoint

---
+++ SingleViewApi/V1/Boundary/Response/CouncilTaxSearchResponseObject.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+
+namespace SingleViewApi.V1.Boundary.Response;
+
+public class CouncilTaxSearchResponseObject
+{
+#nullable enable
+    public List<CouncilTaxSearchResponse>? Customers { get; set; }
+#nullable disable
+    public string Error { get; set; }
+}
+
+public class CouncilTaxSearchResponse
+{
+    public string Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+#nullable enable
+    public DateTime? DateOfBirth { get; set; }
+    public string? NiNumber { get; set; }
+#nullable disable
+    public AcademyAddress FullAddress { get; set; }
+    public string PostCode { get; set; }
+}
+
+public class AcademyAddress
+{
+    public string Line1 { get; set; }
+    public string Line2 { get; set; }
+    public string Line3 { get; set; }
+    public string Line4 { get; set; }
+    public string Postcode { get; set; }
+}
--- SingleViewApi/V1/Gateways/AcademyGateway.cs
+++ SingleViewApi/V1/Gateways/AcademyGateway.cs
@@ -2,6 +2,7 @@ using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
+using SingleViewApi.V1.Boundary.Response;
 using SingleViewApi.V1.Gateways.Interfaces;
 
 namespace SingleViewApi.V1.Gateways;
@@ -17,15 +18,15 @@ public class AcademyGateway: IAcademyGateway
         _baseUrl = baseUrl;
     }
 
-    public async Task<dynamic> GetCouncilTaxAccountsByCustomerName(string firstName, string lastName)
+    public async Task<CouncilTaxSearchResponseObject> GetCouncilTaxAccountsByCustomerName(string firstName, string lastName)
     {
         var request = new HttpRequestMessage(HttpMethod.Get,
-            $"{_baseUrl}/search?firstName={firstName}&lastName={lastName}");
+            $"{_baseUrl}/council-tax/search?firstName={firstName}&lastName={lastName}");
 
         var response = await _httpClient.SendAsync(request);
 
 #nullable enable
-        dynamic? results = null;
+        CouncilTaxSearchResponseObject? results = null;
 #nullable disable
 
         if (response.StatusCode == HttpStatusCode.OK)
@@ -33,7 +34,7 @@ public class AcademyGateway: IAcademyGateway
             var jsonBody = response.Content.ReadAsStringAsync().Result;
 
 
-            results = JsonConvert.DeserializeObject<dynamic>(jsonBody);
+            results = JsonConvert.DeserializeObject<CouncilTaxSearchResponseObject>(jsonBody);
 
         }
 
