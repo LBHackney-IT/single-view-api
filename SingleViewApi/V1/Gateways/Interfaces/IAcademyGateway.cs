da2c2688b18915a50be45f76a976970d1a35311c
Charli Wild <charli.wild@charlis-mbp.lan>
2022-06-29 09:12:38 +0100

    added auth header to academy endpoint

--- SingleViewApi/V1/Gateways/Interfaces/IAcademyGateway.cs
+++ SingleViewApi/V1/Gateways/Interfaces/IAcademyGateway.cs
@@ -5,5 +5,5 @@ namespace SingleViewApi.V1.Gateways.Interfaces;
 
 public interface IAcademyGateway
 {
-    Task<CouncilTaxSearchResponseObject> GetCouncilTaxAccountsByCustomerName(string firstName, string lastName);
+    Task<CouncilTaxSearchResponseObject> GetCouncilTaxAccountsByCustomerName(string firstName, string lastName, string userToken);
 }
