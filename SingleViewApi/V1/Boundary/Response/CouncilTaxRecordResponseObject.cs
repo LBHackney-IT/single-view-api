069bc10b119bf8cee78257c0ae42681ce1bb0d4b
Charli Wild <charli.wild@charlis-mbp.lan>
2022-07-11 11:45:09 +0100

    allowed nullable fields to council tax record

--- SingleViewApi/V1/Boundary/Response/CouncilTaxRecordResponseObject.cs
+++ SingleViewApi/V1/Boundary/Response/CouncilTaxRecordResponseObject.cs
@@ -4,22 +4,23 @@ public class CouncilTaxRecordResponseObject
 {
     public int AccountReference { get; set; }
 
-    public string AccountCheckDigit { get; set; }
-
-    public string Title { get; set; }
-
-    public string FirstName { get; set; }
-
-    public string LastName { get; set; }
-
-    public string PaymentMethod { get; set; }
-
-    public decimal AccountBalance { get; set; }
-
-
-    public Address PropertyAddress { get; set; }
 
 #nullable enable
+
+    public string? AccountCheckDigit { get; set; }
+
+    public string? Title { get; set; }
+
+    public string? FirstName { get; set; }
+
+    public string? LastName { get; set; }
+
+    public string? PaymentMethod { get; set; }
+
+    public decimal? AccountBalance { get; set; }
+
+
+    public Address? PropertyAddress { get; set; }
 
     public Address? ForwardingAddress { get; set; }
 
