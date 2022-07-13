eb32a1c8463d28473d7e924b19ef2b146fc231de
Charli Wild <charli.wild@charlis-mbp.lan>
2022-07-13 08:48:17 +0100

    updated address mapping for academy addresses

--- SingleViewApi/V1/Boundary/Address.cs
+++ SingleViewApi/V1/Boundary/Address.cs
@@ -2,9 +2,9 @@ namespace SingleViewApi.V1.Boundary;
 
 public class Address
 {
-    public string Line1 { get; set; }
-    public string Line2 { get; set; }
-    public string Line3 { get; set; }
-    public string Line4 { get; set; }
+    public string AddressLine1 { get; set; }
+    public string AddressLine2 { get; set; }
+    public string AddressLine3 { get; set; }
+    public string AddressLine4 { get; set; }
     public string Postcode { get; set; }
 }
