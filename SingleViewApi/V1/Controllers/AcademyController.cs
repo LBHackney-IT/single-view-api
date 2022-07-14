831c4b70b64b034408976c0d66504e383ceaa17f
Jonathan Lewis <jonathan.lewis@madetech.com>
2022-07-14 15:57:32 +0100

    Refactor Academy controller endpoints to be '/academy/council-tax' and add '/academy/benefits' endpoint - Add controller test for Housing Benefits use case - Add controller method for Housing Benefits use case

--- SingleViewApi/V1/Controllers/AcademyController.cs
+++ SingleViewApi/V1/Controllers/AcademyController.cs
@@ -1,29 +1,25 @@
-using System;
 using SingleViewApi.V1.Boundary.Response;
 using SingleViewApi.V1.UseCase.Interfaces;
 using Hackney.Core.Logging;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using SingleViewApi.V1.Boundary.Request;
 
 namespace SingleViewApi.V1.Controllers
 {
     [ApiController]
-    //TODO: Rename to match the APIs endpoint
-    [Route("api/v1/council-tax")]
+    [Route("api/v1/academy")]
     [Produces("application/json")]
     [ApiVersion("1.0")]
-    //TODO: rename class to match the API name
     public class AcademyController : BaseController
     {
         private readonly IGetCouncilTaxAccountByAccountRefUseCase _getCouncilTaxAccountByAccountRefUseCase;
+        private readonly IGetHousingBenefitsAccountByAccountRefUseCase _getHousingBenefitsAccountByAccountRefUseCase;
 
-
-        public AcademyController(IGetCouncilTaxAccountByAccountRefUseCase getCouncilTaxAccountByAccountRefUseCase)
+        public AcademyController(IGetCouncilTaxAccountByAccountRefUseCase getCouncilTaxAccountByAccountRefUseCase, IGetHousingBenefitsAccountByAccountRefUseCase getHousingBenefitsAccountByAccountRefUseCase)
         {
             _getCouncilTaxAccountByAccountRefUseCase = getCouncilTaxAccountByAccountRefUseCase;
-
+            _getHousingBenefitsAccountByAccountRefUseCase = getHousingBenefitsAccountByAccountRefUseCase;
         }
 
         //TODO: add xml comments containing information that will be included in the auto generated swagger docs (https://github.com/LBHackney-IT/lbh-SingleViewApi/wiki/Controllers-and-Response-Objects)
@@ -33,14 +29,25 @@ namespace SingleViewApi.V1.Controllers
         /// <response code="200">...</response>
         /// <response code="400">Invalid Query Parameter.</response>
         [ProducesResponseType(typeof(CustomerResponseObject), StatusCodes.Status200OK)]
-
-        [HttpGet]
+        [HttpGet("council-tax")]
         [LogCall(LogLevel.Information)]
         public IActionResult GetCouncilTaxAccount([FromQuery] string id, [FromHeader] string authorization)
         {
             return Ok(_getCouncilTaxAccountByAccountRefUseCase.Execute(id, authorization).Result);
         }
 
-
+        //TODO: add xml comments containing information that will be included in the auto generated swagger docs (https://github.com/LBHackney-IT/lbh-SingleViewApi/wiki/Controllers-and-Response-Objects)
+        /// <summary>
+        /// ...
+        /// </summary>
+        /// <response code="200">...</response>
+        /// <response code="400">Invalid Query Parameter.</response>
+        [ProducesResponseType(typeof(CustomerResponseObject), StatusCodes.Status200OK)]
+        [HttpGet("benefits")]
+        [LogCall(LogLevel.Information)]
+        public IActionResult GetHousingBenefitsAccount([FromQuery] string id, [FromHeader] string authorization)
+        {
+            return Ok(_getHousingBenefitsAccountByAccountRefUseCase.Execute(id, authorization).Result);
+        }
     }
 }
