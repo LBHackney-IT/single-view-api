517be363c18e4350f1089fa88a2c741977ca42d0
Charli Wild <charli.wild@charlis-mbp.lan>
2022-07-22 11:05:34 +0100

    cautionary alerts test update

--- SingleViewApi/V1/Domain/CautionaryAlert.cs
+++ SingleViewApi/V1/Domain/CautionaryAlert.cs
@@ -5,15 +5,16 @@ namespace SingleViewApi.V1.Domain;
 
 public class CautionaryAlert
 {
-    public DateTime? DateModified { get; set; }
+    public string DateModified { get; set; }
     public string ModifiedBy { get; set; }
-    public DateTime? StartDate { get; set; }
-    public DateTime? EndDate { get; set; }
+    public string StartDate { get; set; }
+    #nullable enable
+    public string? EndDate { get; set; }
+    #nullable disable
     public string AlertCode { get; set; }
     public string Description { get; set; }
     public string Reason { get; set; }
 }
-
 public class CautionaryAlertResponseObject
 {
     public string PersonId { get; set; }
