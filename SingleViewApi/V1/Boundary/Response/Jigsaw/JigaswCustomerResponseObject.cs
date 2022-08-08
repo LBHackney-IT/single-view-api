63549bf0d51872c75873d9516760372a8a46431f
el <elena@madetech.com>
2022-08-08 10:55:52 +0100

    lint

--- SingleViewApi/V1/Boundary/Response/Jigsaw/JigaswCustomerResponseObject.cs
+++ SingleViewApi/V1/Boundary/Response/Jigsaw/JigaswCustomerResponseObject.cs
@@ -90,13 +90,13 @@ public class PersonInfo
 public class SupportWorker
 {
 #nullable enable
-    public string? FirstName  { get; set; }
-    public string? LastName  { get; set; }
-    public string? FullName  { get; set; }
-    public string? JobTitle  { get; set; }
-    public string? Agency  { get; set; }
-    public string? PhoneNumber  { get; set; }
-    public string? EmailAddress  { get; set; }
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+    public string? FullName { get; set; }
+    public string? JobTitle { get; set; }
+    public string? Agency { get; set; }
+    public string? PhoneNumber { get; set; }
+    public string? EmailAddress { get; set; }
 #nullable disable
 }
 
