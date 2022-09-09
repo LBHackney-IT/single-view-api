3a7fb42e1238036cd4e00e242dd183e730b69af5
Tausif <tausifpatel.dev@gmail.com>
2022-09-09 10:38:18 +0100

    Add JsonProperty attribute

--- SingleViewApi/V1/Boundary/Response/SharedPlanResponseObject.cs
+++ SingleViewApi/V1/Boundary/Response/SharedPlanResponseObject.cs
@@ -1,8 +1,10 @@
 using System.Collections.Generic;
+using Newtonsoft.Json;
 
 namespace SingleViewApi.V1.Boundary.Response;
 
 public class SharedPlanResponseObject
 {
+    [JsonProperty("planIds")]
     public List<string> PlanIds { get; set; }
 }
