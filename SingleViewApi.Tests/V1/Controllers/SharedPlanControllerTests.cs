6a695d8b6fed8a6af5c8f2d7b8486a367dcf85fd
Tausif <tausifpatel.dev@gmail.com>
2022-09-12 09:28:08 +0100

    add tests for sharedPlanController

---
+++ SingleViewApi.Tests/V1/Controllers/SharedPlanControllerTests.cs
@@ -0,0 +1,37 @@
+using AutoFixture;
+using Hackney.Core.Testing.Shared;
+using Moq;
+using NUnit.Framework;
+using SingleViewApi.V1.Boundary.Request;
+using SingleViewApi.V1.Boundary.Response;
+using SingleViewApi.V1.Controllers;
+using SingleViewApi.V1.UseCase.Interfaces;
+
+namespace SingleViewApi.Tests.V1.Controllers;
+
+public class SharedPlanControllerTests : LogCallAspectFixture
+{
+    private SharedPlanController _classUnderTest;
+    private Mock<ICreateSharedPlanUseCase> _mockCreateSharedPlanUseCase;
+    private Fixture _fixture;
+
+    [SetUp]
+    public void SetUp()
+    {
+        _mockCreateSharedPlanUseCase = new Mock<ICreateSharedPlanUseCase>();
+        _classUnderTest = new SharedPlanController(_mockCreateSharedPlanUseCase.Object);
+        _fixture = new Fixture();
+    }
+
+    [Test]
+    public void CreateSharedPlanGetsCalled()
+    {
+        var request = _fixture.Create<CreateSharedPlanRequest>();
+        var response = _fixture.Create<CreateSharedPlanResponseObject>();
+        _mockCreateSharedPlanUseCase.Setup(x => x.Execute(It.IsAny<CreateSharedPlanRequest>())).ReturnsAsync(response);
+
+        _classUnderTest.CreateSharedPlan(request);
+
+        _mockCreateSharedPlanUseCase.Verify(x => x.Execute(It.IsAny<CreateSharedPlanRequest>()), Times.Once);
+    }
+}
