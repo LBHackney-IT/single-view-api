8f657aaa445d5d9e3ea97bb915aaa42d53c10c18
Adam <adam.tracy20@gmail.com>
2024-01-24 14:45:45 +0000

    Fix redis version numbering

--- terraform/development/main.tf
+++ terraform/development/main.tf
@@ -64,7 +64,7 @@ resource "aws_elasticache_cluster" "redis" {
     engine_version       = "7.0"
     node_type            = "cache.t4g.micro"
     num_cache_nodes      = 1
-    parameter_group_name = "default.redis7.1"
+    parameter_group_name = "default.redis7"
     port                 = 6379
     subnet_group_name    = aws_elasticache_subnet_group.default.name
     security_group_ids   = [aws_security_group.redis_sg.id]
