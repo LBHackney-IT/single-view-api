3dd9062685390126c6fa0b328fdf03e2a643845d
Adam <adam.tracy20@gmail.com>
2024-01-24 16:00:14 +0000

    Fix merged record creation with legacy timestamp

--- SingleViewApi/Startup.cs
+++ SingleViewApi/Startup.cs
@@ -57,6 +57,7 @@ namespace SingleViewApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
             services
                 .AddMvc().AddNewtonsoftJson();
             services.AddApiVersioning(o =>
