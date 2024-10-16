378159e3e94382a66da763b82c84053e8ac8a260
Selwyn Preston <Selwyn.Preston@hackney.gov.uk>
2024-10-16 10:49:49 +0100

    include database config for restored database

--- terraform/production/main.tf
+++ terraform/production/main.tf
@@ -88,3 +88,50 @@ terraform {
   }
 }
 
+################################################################################
+# Supporting Resources
+################################################################################
+
+data "aws_ssm_parameter" "uh_postgres_db_password" {
+    name = "/single-view/production/postgres-password"
+}
+
+data "aws_ssm_parameter" "uh_postgres_username" {
+    name = "/single-view/production/postgres-username"
+}
+
+#####
+# DB
+#####
+import {
+  to = module.postgres_db.aws_db_instance.lbh-db
+  id = "singleview-db-production"
+}
+
+module "postgres_db" {
+    source = "github.com/LBHackney-IT/aws-hackney-common-terraform.git//modules/database/postgres"
+    environment_name = "production"
+    vpc_id = local.vpc_id
+    db_identifier = "singleview"
+    db_name = "singleview"
+    db_port  = 5302
+    subnet_ids = data.aws_subnet_ids.all.ids
+    db_engine = "postgres"
+    db_engine_version = "16.3" //DMS does not work well with v12
+    db_instance_class = "db.t3.micro"
+    db_allocated_storage = 20
+    maintenance_window = "sun:10:00-sun:10:30"
+    db_username = data.aws_ssm_parameter.uh_postgres_username.value
+    db_password = data.aws_ssm_parameter.uh_postgres_db_password.value
+    storage_encrypted = false
+    multi_az = false //only true if production deployment
+    publicly_accessible = false
+    project_name = "single view"
+    db_allow_major_version_upgrade = "true"
+    db_parameter_group_name = "postgres16"
+    deletion_protection = true
+    copy_tags_to_snapshot = true
+    additional_tags = {
+        BackupPolicy = "Prod"
+    }
+}
